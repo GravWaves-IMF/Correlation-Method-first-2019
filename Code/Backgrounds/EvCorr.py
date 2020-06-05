0297a2989abef9e08daf94cc25fb8ddffffb15d4
Paolo Marcoccia <marcocciapaolo1991@gmail.com>
2020-06-05 12:38:16 +0200

    Add files via upload

---
+++ Code/Backgrounds/EvCorr.py
@@ -0,0 +1,13 @@
+# Data Settings for the obtained events correlations at time of max correlation
+
+ev1_osc = 0.97274378204
+ev1_rc = 0.5274514754940001
+
+ev2_osc = 0.838004745539
+ev2_rc = 0.0997368942573
+
+ev3_osc = 0.784028592958
+ev3_rc = 0.323771888555
+
+ev4_osc = 0.936826752885
+ev4_rc = 0.701481091866
